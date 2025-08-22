db769942c64fe91820db949359d9f2427c5a6eb1
Amelie Torre Walsh <amelietorrewalsh@gmail.com>
2025-08-22 10:28:13 -0300

    Arranque de TPO

---
+++ main.py
@@ -0,0 +1,7 @@
+#SISTEMA DE VENTA DE ENTRADAS PARA ESPECTÁCULOS
+#Implementar un sistema que gestione las ventas de entradas
+#para distintos espectáculos a realizarse en un estadio, utilizando matrices,
+#listas y diccionarios para mentener la información, almacenandola en archivos 
+# para permitir su posterior recuperación. Aplicar recursividad para realizar las 
+# busquedad de una manera agil y fexlible.
+
